25e5bb9a16116dd8a4026298057e10e87ccc4148
cahyadie <cahyadiilham12@gmail.com>
2025-01-21 21:55:11 +0700

    mengubah code dari path menjadi Query

--- app/src/main/java/com/example/ujianakhir/service/HewanService.kt
+++ app/src/main/java/com/example/ujianakhir/service/HewanService.kt
@@ -1,34 +1,31 @@
 package com.example.ujianakhir.service
 
 import com.example.ujianakhir.model.Hewan
-import com.example.ujianakhir.model.HewanDetailResponse
-import com.example.ujianakhir.model.HewanResponse
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface HewanService {
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json"
     )
-    @GET(".")
-    suspend fun getHewan(): HewanResponse
+    @GET("gethewan.php")
+    suspend fun getHewan(): List<Hewan>
 
-    @GET("{idhewan}")
-    suspend fun getHewanById(@Path("idhewan") idhewan: String): HewanDetailResponse
+    @GET("get1hewan.php")
+    suspend fun getHewanById(@Query("id_hewan") idhewan: String): Hewan
 
-    @POST("store")
+    @POST("inserthewan.php")
     suspend fun insertHewan(@Body hewan: Hewan)
 
-    @PUT("{idhewan}")
-    suspend fun updateHewan(@Path("idhewan") idhewan: String, @Body hewan: Hewan)
+    @PUT("edithewan.php")
+    suspend fun updateHewan(@Query("id_hewan") id_hewan: String, @Body hewan: Hewan)
 
-    @DELETE("{idhewan}")
-    suspend fun deleteHewan(@Path("idhewan") idhewan: String): Response<Void>
+    @DELETE("deletehewan.php")
+    suspend fun deleteHewan(@Query("id_hewan") id_hewan: String): retrofit2.Response<Void>
 }
